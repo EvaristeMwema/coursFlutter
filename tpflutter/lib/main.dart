bb172f43b8cf21c1b7d1667c996237d01a712aa9
EvaristeMwema <evaristemwema@gmail.com>
2022-06-11 12:17:06 +0200

    flutter tp

---
+++ tpflutter/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:tpflutter/personne.dart';
+import 'package:tpflutter/update.dart';
+import 'package:tpflutter/deleteP.dart';
+
+void main() {
+ // runApp(MyApp());
+   runApp(MaterialApp(
+    home: Personne(),
+   ));
+    //runApp(MaterialApp(
+    //home: DeletePersonne(),
+ // ));
+  //   runApp(MaterialApp(
+  //   home: EditPersonne(),
+  // ));
+}
